c7467a8f81803560eb939389959f5272f72291bd
SangeethKumar <sangidsk086@gmail.com>
2025-08-16 07:18:30 +0530

    minute changes like updating quantity stuff like that

--- data/products.js
+++ data/products.js
@@ -1,13 +1,12 @@
-export function getProduct(productId){
+export function getProduct(productId) {
   let matchingProduct;
 
   products.forEach((product) => {
-
-    if(product.id === productId){
+    if (product.id === productId) {
       matchingProduct = product;
     }
-
   });
+
   return matchingProduct;
 }
 
