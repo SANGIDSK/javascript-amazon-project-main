49d4107c3202d26c74b3d65a7de49000d7ed8701
SangeethKumar <sangidsk086@gmail.com>
2025-08-25 08:13:03 +0530

    use iheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -36,8 +36,30 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`;
   }
 
+  extraInfoHTML() {
+    return ``;
+  }
+
 
 }
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+      </a>
+    `;
+  }
+}
+
 
 export const products = [
   {
@@ -699,6 +721,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing (productDetails);
+  }
   return new Product(productDetails);
+  
 });
 
